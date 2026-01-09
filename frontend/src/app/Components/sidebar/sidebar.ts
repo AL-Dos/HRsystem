7743554b4b3e106bc080385989cdcca5d985c993
Admin <omictinaldous@gmail.com>
2026-01-09 17:05:37 +0800

    feat: sidebar

--- frontend/src/app/Components/sidebar/sidebar.ts
+++ frontend/src/app/Components/sidebar/sidebar.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
-
+  @Input() isOpen = true;
+  @Output() toggle = new EventEmitter<void>();
 }
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Footer } from "./Components/footer/footer";
 import { Header } from "./Components/header/header";
+import { Sidebar } from "./Components/sidebar/sidebar";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [Footer, Header],
+  imports: [Footer, Header, Sidebar, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+  sidebarOpen = true;
+
+  toggleSidebar(): void {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
